5cc98dcb3d23a6fa651d5555fdd2bc9769bca7fb
eshark9312 <eshark9312@outlook.com>
2023-09-12 06:54:17 -0500

    fix schedule

--- controllers/user/schedule.js
+++ controllers/user/schedule.js
@@ -1,4 +1,5 @@
-const ScheduleModel = require('../../models/userData/Schedule')
+const mongoose = require("mongoose");
+const ScheduleModel = require("../../models/userData/Schedule");
 
 module.exports = {
   create: async function (req, res) {
@@ -22,13 +23,47 @@ module.exports = {
       });
   },
   getAll: async function (req, res) {
-    let data = await ScheduleModel.find()
+    let data = await ScheduleModel.find();
     res.status(200).json({ message: null, data: data });
   },
   getFilter: async function (req, res) {
-    const filter = req.body;
-    let data = await ScheduleModel.find(filter);
-    res.status(200).json({ message: null, data: data });
+    const USER_ID = new mongoose.Types.ObjectId(req.body.user_id);
+    ScheduleModel.aggregate([
+      {
+        $match: {
+          user_id: USER_ID,
+        },
+      },
+      {
+        $addFields: {
+          collectionName: {
+            $cond: {
+              if: { $eq: ["$MatiereOrItem", "Matiere"] },
+              then: "matieres",
+              else: "items",
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "$collectionName",
+          localField: "matiere_or_item_id",
+          foreignField: "_id",
+          as: "matiere_or_item",
+        },
+      },
+    ])
+      .then(function (result) {
+        res.status(200).json({
+          message: "Found successfully",
+          data: result,
+        });
+      })
+      .catch(function (err) {
+        console.log(err);
+        res.status(400).json({ message: "Not found", data: null });
+      });
   },
   getById: function (req, res) {
     ScheduleModel.findById(req.params.id)
